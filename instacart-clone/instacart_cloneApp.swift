9f5a8875d6c639c0d903b2c3e5e97cf0ce04679a
Anika Morris <anikamorris11@gmail.com>
2020-10-27 18:18:44 -0500

    Initial Commit

---
+++ instacart-clone/instacart_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  instacart_cloneApp.swift
+//  instacart-clone
+//
+//  Created by Anika Morris on 10/27/20.
+//
+
+import SwiftUI
+
+@main
+struct instacart_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
